78073b4be40bbf6b79bd89ab7d42532a268c9e64
NachoLima <lolnacho.lima@gmail.com>
2024-11-29 18:05:56 -0300

    nav updated

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -16,37 +16,67 @@ const Nav = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <Image src="/logo.svg" alt="logo" width={108} height={96} />
-          <div className="text-moss-green text-2xl lg:text-5xl font-Righteous pt-2 lg:pt-8">IBIRADOPTÁ</div>
+          <div className="text-moss-green text-2xl lg:text-5xl font-Righteous pt-2 lg:pt-8">
+            IBIRADOPTÁ
+          </div>
         </div>
         <div className="block lg:hidden">
-          <button onClick={() => setIsOpen(!isOpen)} className="text-moss-green focus:outline-none"> 
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> 
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path> 
-            </svg> 
-          </button> 
-        </div> 
-        <div className={`lg:flex ${isOpen ? 'block' : 'hidden'} w-full lg:w-auto`}> 
-        <ul className="flex flex-col lg:flex-row lg:space-x-10 space-y-2 lg:space-y-0 font-Poppins text-center lg:text-left"> 
-        <li> 
-          <Link href="/" className="text-moss-green block px-4 py-2 text-lg lg:text-2xl hover:text-green-700">Home</Link> 
-          </li> 
-          <li> 
-            <Link href="#" className="text-moss-green block px-4 py-2 text-lg lg:text-2xl hover:text-green-700">Explorar</Link> 
-          </li> 
-            {!session ? ( 
-          <> 
-          <li> 
-            <RegisterButton /> 
-          </li> 
-          <li> 
-            <Login /> 
-          </li> 
-          </>
-          ) : ( 
-            <li> 
-              <Logout /> 
-            </li> 
-          )} 
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-moss-green focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`lg:flex ${
+            isOpen ? "block" : "hidden"
+          } w-full lg:w-auto`}
+        >
+          <ul className="flex flex-col lg:flex-row lg:space-x-10 space-y-2 lg:space-y-0 font-Poppins text-center lg:text-left">
+            <li>
+              <Link
+                href="/"
+                className="text-moss-green block px-4 py-2 text-lg lg:text-2xl hover:text-green-700"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/explorar" // Add this link to navigate to the new page
+                className="text-moss-green block px-4 py-2 text-lg lg:text-2xl hover:text-green-700"
+              >
+                Explorar
+              </Link>
+            </li>
+            {!session ? (
+              <>
+                <li>
+                  <RegisterButton />
+                </li>
+                <li>
+                  <Login />
+                </li>
+              </>
+            ) : (
+              <li>
+                <Logout />
+              </li>
+            )}
           </ul>
         </div>
       </div>
@@ -55,4 +85,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
